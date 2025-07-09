6d8228da52a81455db782908a738c38f53405b95
Rumpa Das <164724970+rumpadas1@users.noreply.github.com>
2025-07-09 16:48:44 +0530

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 
 const geistSans = Geist({
   subsets: ["latin"],
